59689769a2591770e36be188bf311e6f90ee0ff4
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-14 20:25:19 +0545

    Reverse, Compare and Auto Clearing stack

--- Day_8/PalindromeChecker.java
+++ Day_8/PalindromeChecker.java
@@ -2,30 +2,55 @@ package Day_8;
 
 import java.util.Scanner;
 
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Stack {
+    Node top;
+
+    // Push Operation
+    void push(char data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    // Pop Operation
+    char pop() {
+        if (top == null) {
+            return 0;
+        }
+        char value = top.data;
+        top = top.next;
+        return value;
+    }
+
+    // Empty or not Operation
+    boolean isEmpty() {
+        return top == null;
+    }
+
+    // Stack clear Operation
+    void clear() {
+        top = null;
+    }
+}
+
 public class PalindromeChecker {
-    class Node{
-        char data;
-        Node next;
 
-        Node(char data){
-            this.data = data;
-            this.next = null;
-        }
-    }
-    class Stack{
-        Node top;
-
-        //Push Operation
-        void push(char data){
-            Node newNode = new Node(data);
-            newNode.next = top;
-            top = newNode;
-        }
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Stack stack = new Stack();
+        String input;
 
-        while(true){
+        while (true) {
             System.out.println("---Palindrome Checker---");
             System.out.println("1. Check Palindrome");
 
@@ -34,13 +59,35 @@ public class PalindromeChecker {
 
             switch (choice) {
                 case 1:
+                    stack.clear();
                     System.out.println("Enter a word : ");
+                    input = sc.nextLine();
+
+                    // Pushing characters into the stack
+                    for (int i = 0; i < input.length(); i++) {
+                        stack.push(input.charAt(i));
+                    }
+
+                    boolean isPalindrome = true;
+
+                    // Comparing characters
+                    for (int i = 0; i < input.length(); i++) {
+                        if (input.charAt(i) != stack.pop()) {
+                            isPalindrome = false;
+                            break;
+                        }
+                    }
+
+                    if (isPalindrome) {
+                        System.out.println("It is a Palindrome.");
+                    } else {
+                        System.out.println("It is not a Palindrome.");
+                    }
                     break;
-            
+
                 default:
                     System.out.println("Invalid choice. Try again.");
             }
         }
-        
     }
 }
