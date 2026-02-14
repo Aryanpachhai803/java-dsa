9f70213b907585926dde9161a4c5420d1fe10d0f
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-14 20:08:50 +0545

    Pushing word into the stack

--- Day_8/PalindromeChecker.java
+++ Day_8/PalindromeChecker.java
@@ -3,8 +3,44 @@ package Day_8;
 import java.util.Scanner;
 
 public class PalindromeChecker {
+    class Node{
+        char data;
+        Node next;
+
+        Node(char data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    class Stack{
+        Node top;
+
+        //Push Operation
+        void push(char data){
+            Node newNode = new Node(data);
+            newNode.next = top;
+            top = newNode;
+        }
+    }
     public static void main(String[] args) {
-        PalindromeChecker pc = new PalindromeChecker();
         Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("---Palindrome Checker---");
+            System.out.println("1. Check Palindrome");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter a word : ");
+                    break;
+            
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        }
+        
     }
 }
