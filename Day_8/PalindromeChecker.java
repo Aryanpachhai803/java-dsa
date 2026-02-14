099855ffbf0f3a165691a8e8673dd48d0159880b
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-14 19:55:06 +0545

    Palindrome Checker

---
+++ Day_8/PalindromeChecker.java
@@ -0,0 +1,10 @@
+package Day_8;
+
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        PalindromeChecker pc = new PalindromeChecker();
+        Scanner sc = new Scanner(System.in);
+    }
+}
