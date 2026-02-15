83f39592c6ec23d1c1bdd1ce6b58ebc20c6e8ba5
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-15 18:23:46 +0545

    Initialization of Booking System

---
+++ Day_5/TicketBooking.java
@@ -0,0 +1,13 @@
+package Day_5;
+
+import java.util.Scanner;
+
+public class TicketBooking {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            int choice = sc.nextInt();
+        }
+    }
+}
