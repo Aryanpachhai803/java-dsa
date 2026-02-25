c78c3089483ee857df8e5e74d3a22221b12fb81f
Aryanpachhai <aryanpachhai803@gmail.com>
2026-02-25 17:48:49 +0545

    Initialization

--- Day_9/QueueUsingLinkedList.java
+++ Day_9/QueueUsingLinkedList.java
@@ -1,10 +1,50 @@
 package Day_9;
 
+import java.util.Scanner;
 
 public class QueueUsingLinkedList {
+    int size = 0;
+    int MAX = 10;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node front;
+    Node rear;
+
+    // Constructor
+    public QueueUsingLinkedList() {
+        this.front = null;
+        this.rear = null;
+    }
 
 
     public static void main(String[] args) {
-        
+        QueueUsingLinkedList queue = new QueueUsingLinkedList();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("===== Queue Menu =====");
+            System.out.println("1. Enqueue");
+            System.out.println("2. Dequeue");
+            System.out.println("3. Peek");
+            System.out.println("4. Display");
+            System.out.println("5. Exit");
+
+            int choice = sc.nextInt();
+
+            switch (choice) {
+
+                default:
+                    System.out.println("Invalid choice. Try again");
+            }
+        }
     }
 }
