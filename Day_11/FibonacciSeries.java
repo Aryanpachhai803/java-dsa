f969a2ea4f8d5132b647579859824963675fee5c
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-11 23:12:45 +0545

    Recursion Fibonacci Series

---
+++ Day_11/FibonacciSeries.java
@@ -0,0 +1,16 @@
+package Day_11;
+
+public class FibonacciSeries {
+    static int fib(int n){
+        if(n <= 1){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+    public static void main(String[] args) {
+        int num = 5;
+        for(int i=0;i<num;i++){
+            System.out.println(fib(i));
+        }
+    }
+}
