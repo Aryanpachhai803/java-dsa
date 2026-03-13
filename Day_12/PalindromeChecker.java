76d4551ef08f8c1550d58eb37119e31d0a06ab56
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-13 20:39:03 +0545

    Base call and Recursive call

--- Day_12/PalindromeChecker.java
+++ Day_12/PalindromeChecker.java
@@ -1,7 +1,20 @@
 package Day_12;
+import java.util.Scanner;
 
 public class PalindromeChecker {
+
+    static boolean Palindrome(String s,int start,int end){
+        if(start>=end){
+            return true;
+        }
+        if(s.charAt(start) != s.charAt(end)){
+            return false;
+        }
+        return Palindrome(s, start +1, end-1);
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+
     }
 }
