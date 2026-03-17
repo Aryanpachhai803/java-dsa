376b1c9500b8fb23388a1b0743a582dbcabbd975
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-17 21:24:46 +0545

    Main method

--- Day_12/PalindromeChecker.java
+++ Day_12/PalindromeChecker.java
@@ -11,7 +11,7 @@ public class PalindromeChecker {
         }
         return Palindrome(s, start +1, end-1);
     }
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a word:");
--- Day_12/SumOfDigits.java
+++ Day_12/SumOfDigits.java
@@ -1,4 +1,5 @@
 package Day_12;
+import java.util.Scanner;
 
 public class SumOfDigits {
     static int sumDigits(int n){
@@ -8,6 +9,14 @@ public class SumOfDigits {
         return (n%10)+  sumDigits(n/10);
     }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        int result = sumDigits(num);
+        System.out.println("Sum of digits: " + result);
+
+        sc.close();
     }
 }
