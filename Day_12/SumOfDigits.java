0f240255a876931f1f08ad07f9759a1bc9135606
Aryanpachhai <aryanpachhai803@gmail.com>
2026-03-17 21:22:16 +0545

    Base and recusrion case

--- Day_12/SumOfDigits.java
+++ Day_12/SumOfDigits.java
@@ -1,6 +1,12 @@
 package Day_12;
 
 public class SumOfDigits {
+    static int sumDigits(int n){
+        if(n == 0){
+            return 0;
+        }
+        return (n%10)+  sumDigits(n/10);
+    }
     public static void main(String[] args) {
         
     }
