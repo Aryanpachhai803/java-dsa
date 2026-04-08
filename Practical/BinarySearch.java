1501b158f3eb124ed53f6d59bc5a3e1921077f80
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-08 16:17:53 +0545

    Implementation of Binary Search

---
+++ Practical/BinarySearch.java
@@ -0,0 +1,31 @@
+package Practical;
+
+public class BinarySearch {
+    public static int search(int[] arr, int key) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == key)
+                return mid;
+            else if (arr[mid] < key)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1; // not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50}; // must be sorted
+        int key = 40;
+
+        int result = search(arr, key);
+
+        if (result != -1)
+            System.out.println("Element found at index: " + result);
+        else
+            System.out.println("Element not found");
+    }
+}
