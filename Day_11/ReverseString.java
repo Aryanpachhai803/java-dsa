437d3b44bc4f0993b78aab8263d20ae16a57e6ea
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-09 21:44:30 +0545

    Reversing a string base condition

---
+++ Day_11/ReverseString.java
@@ -0,0 +1,12 @@
+package Day_11;
+
+public class ReverseString {
+    static String reverse(String str) {
+        if (str.isEmpty()) {
+            return str;
+        }
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+    public static void main(String[] args) {
+    }
+}
