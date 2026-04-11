5922702e7d9cc1f6d36780073155a3dc3840b916
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-11 22:10:52 +0545

    Capacity in queue

---
+++ Day_4/TrafficControl.java
@@ -0,0 +1,20 @@
+package Day_4;
+
+class CircularQueue {
+    private String[] queue;
+    private int front, rear, size, capacity;
+
+    public CircularQueue(int capacity) {
+        this.capacity = capacity;
+        queue = new String[capacity];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+}
+
+public class TrafficControl {
+    public static void main(String[] args) {
+        }
+    }
