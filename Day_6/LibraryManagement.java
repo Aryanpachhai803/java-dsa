2cf862f3f40ce543e57fc579193922a137170a4d
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-12 22:29:38 +0545

    Book Data

---
+++ Day_6/LibraryManagement.java
@@ -0,0 +1,22 @@
+package Day_6;
+import java.util.Scanner;
+
+class Book {
+    int id;
+    String title;
+    String author;
+    Book next;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.next = null;
+    }
+}
+
+
+public class LibraryManagement {
+    public static void main(String[] args) {
+    }
+}
