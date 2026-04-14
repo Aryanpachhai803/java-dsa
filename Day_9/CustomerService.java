0300e720ad841c1f449a69d412f5ae19fddc3439
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-14 21:24:24 +0545

    Node initilize

---
+++ Day_9/CustomerService.java
@@ -0,0 +1,19 @@
+package Day_9;
+
+import java.util.Scanner;
+
+class Node {
+    String name;
+    Node next;
+
+    Node(String name) {
+        this.name = name;
+        this.next = null;
+    }
+}
+
+
+public class CustomerService {
+    public static void main(String[] args) {
+    }
+}
