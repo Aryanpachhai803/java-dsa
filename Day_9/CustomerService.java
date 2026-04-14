449b41dc68e3ed7ae54baeee206cfe2888103c8a
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-14 21:26:53 +0545

    Insertion and Deletion

--- Day_9/CustomerService.java
+++ Day_9/CustomerService.java
@@ -12,6 +12,36 @@ class Node {
     }
 }
 
+class CustomerQueue {
+    Node front, rear;
+
+    void enqueue(String name) {
+        Node newNode = new Node(name);
+        if (rear == null) {
+            front = rear = newNode;
+            System.out.println(name + " added to queue.");
+            return;
+        }
+
+        rear.next = newNode;
+        rear = newNode;
+        System.out.println(name + " added to queue.");
+    }
+
+    void dequeue() {
+        if (front == null) {
+            System.out.println("Queue is empty.");
+            return;
+        }
+        System.out.println(front.name + " is being served.");
+        front = front.next;
+
+        if (front == null) {
+            rear = null;
+        }
+    }
+
+}
 
 public class CustomerService {
     public static void main(String[] args) {
