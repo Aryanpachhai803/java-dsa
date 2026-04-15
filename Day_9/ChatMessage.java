a2d5a4c3b2f05f98c8c220ba956590344e5ba8e2
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-15 21:03:36 +0545

    Nodes

---
+++ Day_9/ChatMessage.java
@@ -0,0 +1,20 @@
+package Day_9;
+
+import java.util.Scanner;
+
+class Node {
+    String message;
+    Node next;
+
+    Node(String message) {
+        this.message = message;
+        this.next = null;
+    }
+}
+
+
+public class ChatMessage {
+    public static void main(String[] args) {
+      
+    }
+}
