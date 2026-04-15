e37bed5906a77292672e8e890e78abfc876a414a
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-15 21:04:24 +0545

    Send Message

--- Day_9/ChatMessage.java
+++ Day_9/ChatMessage.java
@@ -12,6 +12,21 @@ class Node {
     }
 }
 
+class ChatQueue {
+    Node front, rear;
+    void sendMessage(String msg) {
+        Node newNode = new Node(msg);
+
+        if (rear == null) {
+            front = rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+        System.out.println("Message Sent: " + msg);
+    }
+
+}
 
 public class ChatMessage {
     public static void main(String[] args) {
