b34c2066a626dcaa6174b87c2b98d703974b0d1a
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-18 21:31:50 +0545

    Insertion

--- Practical/HeapDemo.java
+++ Practical/HeapDemo.java
@@ -5,6 +5,26 @@ public class HeapDemo {
     static String[] task = new String[10]; 
     static int size = 0;
 
+    static void insert(int p, String t) {
+        priority[size] = p;
+        task[size] = t;
+
+        int i = size;
+        size++;
+
+        while (i > 0 && priority[(i - 1) / 2] > priority[i]) {
+            int tempP = priority[i];
+            priority[i] = priority[(i - 1) / 2];
+            priority[(i - 1) / 2] = tempP;
+
+            String tempT = task[i];
+            task[i] = task[(i - 1) / 2];
+            task[(i - 1) / 2] = tempT;
+
+            i = (i - 1) / 2;
+        }
+    }
+
 
 
     public static void main(String[] args) {
