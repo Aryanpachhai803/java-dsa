9ffc25e5614c13c6c5df4b94544b7bcc75676d43
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-18 21:31:12 +0545

    Initialize Priority of task

---
+++ Practical/HeapDemo.java
@@ -0,0 +1,13 @@
+package Practical;
+
+public class HeapDemo {
+    static int[] priority = new int[10];   
+    static String[] task = new String[10]; 
+    static int size = 0;
+
+
+
+    public static void main(String[] args) {
+        
+    }
+}
