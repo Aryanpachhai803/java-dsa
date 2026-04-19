36462d32d1accaee7c8d68785dbc91d5dfd02afb
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-19 21:17:27 +0545

    Initialization of guessing game using Binary search

---
+++ Practical/BinaryDemoGame.java
@@ -0,0 +1,14 @@
+package Practical;
+import java.util.Scanner;
+
+public class BinaryDemoGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int low = 1;
+        int high = 100;
+        int guess;
+        char response;
+
+    }
+}
