bdf6be412f4668e0550073e827ad4a10c68a1c97
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-20 23:02:12 +0545

    Initialize variables

---
+++ Practical/SequentialDemo.java
@@ -0,0 +1,18 @@
+package Practical;
+
+import java.util.Scanner;
+
+class Contact {
+    String name;
+    String phone;
+
+    Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+}
+
+public class SequentialDemo {
+    public static void main(String[] args) {
+    }
+}
