11a0da97ea812f6e24a339f30f98d968dd1e687b
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-20 23:03:39 +0545

    Data insertion

--- Practical/SequentialDemo.java
+++ Practical/SequentialDemo.java
@@ -24,5 +24,28 @@ public class SequentialDemo {
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Contact[] contacts = {
+            new Contact("Alex", "9841000001"),
+            new Contact("Birat", "9841000002"),
+            new Contact("Chintan", "9841000003"),
+            new Contact("David", "9841000004")
+        };
+
+        System.out.print("Enter name to search: ");
+        String name = sc.nextLine();
+
+        int result = searchContact(contacts, name);
+
+        if (result != -1) {
+            System.out.println("Contact Found!");
+            System.out.println("Name: " + contacts[result].name);
+            System.out.println("Phone: " + contacts[result].phone);
+        } else {
+            System.out.println("Contact not found.");
+        }
+
+        sc.close();
     }
 }
