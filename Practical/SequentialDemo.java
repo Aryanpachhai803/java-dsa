168fba06bab74b267544bb00d161c029e2fd6775
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-20 23:03:11 +0545

    Search

--- Practical/SequentialDemo.java
+++ Practical/SequentialDemo.java
@@ -13,6 +13,16 @@ class Contact {
 }
 
 public class SequentialDemo {
+
+    public static int searchContact(Contact[] contacts, String target) {
+        for (int i = 0; i < contacts.length; i++) {
+            if (contacts[i].name.equalsIgnoreCase(target)) {
+                return i; 
+            }
+        }
+        return -1; 
+    }
+
     public static void main(String[] args) {
     }
 }
