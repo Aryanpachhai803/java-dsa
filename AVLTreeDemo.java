988235e49545e34f5e096942ab8dc362ffb9a144
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-21 14:59:50 +0545

    Insertion

--- AVLTreeDemo.java
+++ AVLTreeDemo.java
@@ -47,6 +47,40 @@ class AVLTree {
         return y;
     }
 
+    FileNode insert(FileNode node, String name) {
+        if (node == null)
+            return new FileNode(name);
+
+        if (name.compareTo(node.fileName) < 0)
+            node.left = insert(node.left, name);
+        else if (name.compareTo(node.fileName) > 0)
+            node.right = insert(node.right, name);
+        else
+            return node; 
+
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+
+        int balance = getBalance(node);
+
+        if (balance > 1 && name.compareTo(node.left.fileName) < 0)
+            return rightRotate(node);
+
+        if (balance < -1 && name.compareTo(node.right.fileName) > 0)
+            return leftRotate(node);
+
+        if (balance > 1 && name.compareTo(node.left.fileName) > 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        if (balance < -1 && name.compareTo(node.right.fileName) < 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+
+        return node;
+    }
+
 }
 
 public class AVLTreeDemo {
