85f044f302d2fe382fd8e8df8ee0ba00b9012c7b
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-22 21:46:37 +0545

    Sorting by marks

--- StudentSorter.java
+++ StudentSorter.java
@@ -14,6 +14,52 @@ class Student {
 
 public class StudentSorter {
 
+    public static void mergeSortByMarks(Student[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSortByMarks(arr, left, mid);
+            mergeSortByMarks(arr, mid + 1, right);
+
+            mergeByMarks(arr, left, mid, right);
+        }
+    }
+
+    public static void mergeByMarks(Student[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        Student[] L = new Student[n1];
+        Student[] R = new Student[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i].marks <= R[j].marks) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k++] = L[i++];
+        }
+
+        while (j < n2) {
+            arr[k++] = R[j++];
+        }
+    }
+
+
     public static void main(String[] args) {
     }
 }
