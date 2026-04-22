476f33bd274dcc05ecc27bda3c3337cfa90f3458
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-22 21:47:20 +0545

    Sorting by name

--- StudentSorter.java
+++ StudentSorter.java
@@ -59,6 +59,57 @@ public class StudentSorter {
         }
     }
 
+    public static void mergeSortByName(Student[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSortByName(arr, left, mid);
+            mergeSortByName(arr, mid + 1, right);
+
+            mergeByName(arr, left, mid, right);
+        }
+    }
+
+    public static void mergeByName(Student[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        Student[] L = new Student[n1];
+        Student[] R = new Student[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i].name.compareToIgnoreCase(R[j].name) <= 0) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k++] = L[i++];
+        }
+
+        while (j < n2) {
+            arr[k++] = R[j++];
+        }
+    }
+
+    public static void printStudents(Student[] arr) {
+        System.out.println("\nRoll\tName\tMarks");
+        for (Student s : arr) {
+            System.out.println(s.roll + "\t" + s.name + "\t" + s.marks);
+        }
+    }
 
     public static void main(String[] args) {
     }
