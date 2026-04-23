26a28e8e1245976b0f4f8fe77744495d6250e47a
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-23 20:55:11 +0545

    Expression logic to check balanced or not

--- Day_1/BalancedParentheses.java
+++ Day_1/BalancedParentheses.java
@@ -4,6 +4,34 @@ import java.util.Stack;
 
 public class BalancedParentheses {
 
+    public static boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : expression.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+
+            else if (ch == ')' || ch == '}' || ch == ']') {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
     public static void main(String[] args) {
         
     }
