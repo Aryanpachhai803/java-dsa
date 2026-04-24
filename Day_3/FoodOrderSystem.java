d42a69ad13794ff9d4144cbc40dbd14e44ff9a25
Aryanpachhai <aryanpachhai803@gmail.com>
2026-04-24 21:28:23 +0545

    Order id and item initialize

--- Day_3/FoodOrderSystem.java
+++ Day_3/FoodOrderSystem.java
@@ -2,6 +2,19 @@ package Day_3;
 
 import java.util.*;
 
+class Order {
+    int orderId;
+    String foodItem;
+
+    Order(int orderId, String foodItem) {
+        this.orderId = orderId;
+        this.foodItem = foodItem;
+    }
+
+    public String toString() {
+        return "Order ID: " + orderId + ", Item: " + foodItem;
+    }
+}
 
 public class FoodOrderSystem {
     public static void main(String[] args) {
